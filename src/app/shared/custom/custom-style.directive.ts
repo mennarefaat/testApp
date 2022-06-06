54c61910441e1e63ee67ae4ac918abd33c57cdeb
Menna Refaat <menna.ref3t@gmail.com>
2022-06-06 19:44:05 +0200

    custom directive

--- src/app/shared/custom/custom-style.directive.ts
+++ src/app/shared/custom/custom-style.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appCustomStyle]'
+  selector: '[appCustomStyle]',
 })
-export class CustomStyleDirective {
+export class CustomStyleDirective implements OnChanges {
+  @Input('appCustomStyle') color = 'pink';
+  @Input() hcolor = 'blue';
 
-  constructor() { }
+  @HostListener('mouseenter')
+  hoverColor() {
+    this._element.nativeElement.style.backgroundColor = this.hcolor;
+  }
 
+  @HostListener('mouseleave')
+  noHoverColor() {
+    this._element.nativeElement.style.backgroundColor = this.color;
+  }
+
+  constructor(private _element: ElementRef) {}
+  ngOnChanges(changes: SimpleChanges): void {
+    this._element.nativeElement.style.backgroundColor = this.color;
+  }
 }
